5c10e488fb0ab85c3b03f46974a67e567c1fff33
Chaz Leong <cleong14@gmail.com>
2015-11-05 21:46:39 -1000

    all tests passing

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,27 @@
 'use strict';
 module.exports = function() {
-  
+  var currentNumber;
+
+  return function () {
+    if (!currentNumber) {
+      currentNumber = Math.floor(Math.random() * 1000000 + 1) + 0;
+    }
+    return currentNumber;
+  };
 };
+
+// THESE 2 THINGS DO THE SAME THING
+
+// 'use strict';
+// module.exports = function() {
+//   var currentNumber;
+
+//   var secretNumberGenerator = function () {
+//     if (!currentNumber) {
+//       currentNumber = Math.floor(Math.random() * 1000000 + 1) + 0;
+//     }
+//     return currentNumber;
+//   };
+  
+//   return secretNumberGenerator;
+// };
